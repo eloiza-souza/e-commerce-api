41232f1a760503482daed6964f8ac6582f44bfd2
Eloiza <eloiza.souza@zup.com.br>
2025-01-29 14:21:15 -0300

    feat: add method existsByName

--- src/main/java/com/zup/e_commerce/repositories/ProductRepository.java
+++ src/main/java/com/zup/e_commerce/repositories/ProductRepository.java
@@ -4,6 +4,5 @@ import com.zup.e_commerce.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    boolean existsById(Long id);
-    void deleteById(Long id);
+    boolean existsByName(String name);
 }

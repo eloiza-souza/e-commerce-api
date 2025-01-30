a8322d9b50c61708e0ee9b5506061d7f852e732d
Eloiza <eloiza.souza@zup.com.br>
2025-01-30 13:46:36 -0300

    feat: add method findByName to ProductRepository

--- src/main/java/com/zup/e_commerce/repositories/ProductRepository.java
+++ src/main/java/com/zup/e_commerce/repositories/ProductRepository.java
@@ -3,6 +3,9 @@ package com.zup.e_commerce.repositories;
 import com.zup.e_commerce.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByName(String name);
+    Optional<Product> findByName(String name);
 }

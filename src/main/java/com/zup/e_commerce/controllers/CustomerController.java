8b98d9170cfbe74815ae87b6aaa96cd9027aab1e
Eloiza <eloiza.souza@zup.com.br>
2025-01-31 14:00:05 -0300

    refactor: add annotations em class to configure Swagger

--- src/main/java/com/zup/e_commerce/controllers/CustomerController.java
+++ src/main/java/com/zup/e_commerce/controllers/CustomerController.java
@@ -3,6 +3,9 @@ package com.zup.e_commerce.controllers;
 import com.zup.e_commerce.dtos.CustomerRequest;
 import com.zup.e_commerce.dtos.CustomerResponse;
 import com.zup.e_commerce.services.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,22 +28,37 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping
+    @Operation(summary = "Registrar um novo cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados da requisição inválidos")
+    })
     public ResponseEntity<CustomerResponse> registerCustomer(@Valid @RequestBody CustomerRequest customerRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.addCustomer(customerRequest));
     }
 
     @GetMapping("/{cpf}")
+    @Operation(summary = "Obter cliente pelo CPF")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente encontrado"),
+            @ApiResponse(responseCode = "400", description = "Formato de CPF inválido"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado")
+    })
     public ResponseEntity<CustomerResponse> getConsumerByCpf(
             @PathVariable("cpf") @CPF (message = "CPF inválido") String cpf){
         return ResponseEntity.ok(customerService.getCustomerByCpf(cpf));
     }
 
     @PutMapping("/{cpf}")
+    @Operation(summary = "Atualizar cliente pelo CPF")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados da requisição ou formato de CPF inválidos"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado")
+    })
     public ResponseEntity<CustomerResponse> updateProduct(
             @PathVariable("cpf") @CPF (message = "CPF inválido") String cpf,
             @Valid @RequestBody CustomerRequest customerRequest){
         return ResponseEntity.ok(customerService.updateCustomer(cpf, customerRequest));
     }
-
-
 }

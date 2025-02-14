ab020d436ff02392ffa0056a36acf3f9cdfbac85
Eloiza <eloiza.souza@zup.com.br>
2025-02-14 13:28:10 -0300

    test: add tests to addProduct method

--- src/test/java/com/zup/e_commerce/services/ProductServiceImplTest.java
+++ src/test/java/com/zup/e_commerce/services/ProductServiceImplTest.java
@@ -1,6 +1,11 @@
 package com.zup.e_commerce.services;
 
+import com.zup.e_commerce.dtos.ProductRequest;
+import com.zup.e_commerce.dtos.ProductResponse;
+import com.zup.e_commerce.exceptions.DuplicateFieldException;
 import com.zup.e_commerce.exceptions.ProductNotFoundException;
+import com.zup.e_commerce.mappers.ProductMapper;
+import com.zup.e_commerce.models.Product;
 import com.zup.e_commerce.repositories.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,14 +14,16 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ProductServiceImplTest {
 
     @Mock
     private ProductRepository productRepository;
+
+    @Mock
+    private ProductMapper productMapper;
 
     @InjectMocks
     private ProductServiceImpl productService;
@@ -24,6 +31,50 @@ public class ProductServiceImplTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenProductNameExistsIfAddingProductShouldThrowDuplicateFieldException() {
+        // Given
+        String productName = "Produto Teste";
+        ProductRequest productRequest = new ProductRequest(productName, 8.00, 100);
+        when(productRepository.existsByName(productName)).thenReturn(true);
+        //when
+        DuplicateFieldException exception = assertThrows(DuplicateFieldException.class,
+                () -> productService.addProduct(productRequest));
+
+        assertEquals("O produto com o nome \"" + productName + "\" já existe.", exception.getMessage());
+
+        //then
+        verify(productRepository).existsByName(productName);
+        verify(productMapper, never()).toEntity(any(ProductRequest.class));
+        verify(productRepository, never()).save(any(Product.class));
+        verify(productMapper, never()).toResponse(any(Product.class));
+    }
+
+    @Test
+    void whenProductNameDoesNotExistIfAddingProductShouldSaveAndReturnProductResponse() {
+        // Given
+        String productName = "Produto Teste";
+        ProductRequest productRequest = new ProductRequest(productName, 8.99, 100);
+        Product product = new Product(productName, 8.99, 100);
+        ProductResponse productResponse = new ProductResponse(1L, productName, 8.99, 100);
+
+        when(productRepository.existsByName(productName)).thenReturn(false);
+        when(productMapper.toEntity(productRequest)).thenReturn(product);
+        when(productRepository.save(product)).thenReturn(product);
+        when(productMapper.toResponse(product)).thenReturn(productResponse);
+
+        // When
+        ProductResponse result = productService.addProduct(productRequest);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(productResponse, result);
+        verify(productRepository).existsByName(productName);
+        verify(productMapper).toEntity(productRequest);
+        verify(productRepository).save(product);
+        verify(productMapper).toResponse(product);
     }
     
     @Test
@@ -36,10 +87,8 @@ public class ProductServiceImplTest {
         assertThrows(ProductNotFoundException.class, () -> productService.deleteProduct(id));
 
         //then
-        verify(productRepository, Mockito.times(1))
-                .existsById(id);
-        verify(productRepository, Mockito.never())
-                .deleteById(id);
+        verify(productRepository).existsById(id);
+        verify(productRepository, Mockito.never()).deleteById(id);
     }
 
     @Test
@@ -52,9 +101,7 @@ public class ProductServiceImplTest {
         productService.deleteProduct(id);
 
         //then
-        verify(productRepository, Mockito.times(1))
-                .existsById(id);
-        verify(productRepository, Mockito.times(1))
-                .deleteById(id);
+        verify(productRepository).existsById(id);
+        verify(productRepository).deleteById(id);
     }
 }
